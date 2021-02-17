ef0a9f682cbb6d18a37a27175b4ee8304643a25a
maather-alshablan <63198807+maather-alshablan@users.noreply.github.com>
2021-02-17 16:55:09 +0300

    Enlist updated

--- components/Input/index.js
+++ components/Input/index.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { View, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+
 export default function Input(props) {
   return (
     <View
-      style={{
+      style={[{
         flexDirection: 'row-reverse',
         marginBottom: 20,
-      }}>
-      <Icon name={props.iconName} color={'#01b753'} size={25} />
+        alignItems: 'center'
+      }, props.containerStyle]}>
+      <Icon name={props.iconName} color={'#01b753'} size={25} style={{ marginLeft: 8 }} />
       <TextInput
-        style={{
+        style={[{
           paddingHorizontal: 10,
           textAlign: 'right',
           color: 'black',
@@ -23,11 +25,13 @@ export default function Input(props) {
           borderStartColor: 'white',
           borderEndColor: 'white',
           borderTopColor: 'white'
-        }}
+        }, props.style]}
         placeholder={props.placeholder}
         secureTextEntry={props.secureTextEntry}
         onChangeText={props.onChangeText}
         value={props.value}
+        {...props.textProps}
+
 
       />
 
